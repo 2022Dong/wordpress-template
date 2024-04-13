11278fe94f85c9be00fbd60cea4f51eaaafed9a1
2022Dong <faithhopelove777111@gmail.com>
2024-04-13 22:40:17 +0800

    enqueue scripts & wp_footer()

--- functions.php
+++ functions.php
@@ -1,9 +1,10 @@
 <?php
 
+// wp_head()
 function mytheme_register_styles()
 {
     $version = wp_get_theme()->get('Version');
-    // Theme CSS
+    // Theme CSS  *depending on bootstrap
     wp_enqueue_style('mytheme-style', get_template_directory_uri() . "/style.css", array('mytheme-bootstrap'), $version, 'all');
     // Bootstrap CSS
     wp_enqueue_style('mytheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
@@ -12,3 +13,14 @@ function mytheme_register_styles()
 }
 
 add_action('wp_enqueue_scripts', 'mytheme_register_styles');
+
+// wp_footer()
+function mytheme_register_scripts()
+{
+    wp_enqueue_script('mytheme-jquery', "https://code.jquery.com/jquery-3.4.1.slim.min.js", array(), '3.4.1', 'all', true);
+    wp_enqueue_script('mytheme-popper', "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js", array(), '1.16.0', 'all', true);
+    wp_enqueue_script('mytheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", array(), '4.4.1', 'all', true);
+    wp_enqueue_script('mytheme-main', get_template_directory_uri() . "/assets/js/main.js", array(), '1.0', 'all', true);
+}
+
+add_action('wp_enqueue_scripts', 'mytheme_register_scripts');
