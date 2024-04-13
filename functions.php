6c4e03de563fc81b69383e5d303a1a4267a816ff
2022Dong <faithhopelove777111@gmail.com>
2024-04-13 22:03:13 +0800

    wp_head() & dynamic versioning

--- functions.php
+++ functions.php
@@ -2,7 +2,13 @@
 
 function mytheme_register_styles()
 {
-    wp_enqueue_style('mytheme-bootstrap', get_template_directory_uri() . "/style.css", array(), '1.0', 'all');
+    $version = wp_get_theme()->get('Version');
+    // Theme CSS
+    wp_enqueue_style('mytheme-style', get_template_directory_uri() . "/style.css", array('mytheme-bootstrap'), $version, 'all');
+    // Bootstrap CSS
+    wp_enqueue_style('mytheme-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all');
+    // FontAwesome CSS
+    wp_enqueue_style('mytheme-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all');
 }
 
 add_action('wp_enqueue_scripts', 'mytheme_register_styles');
