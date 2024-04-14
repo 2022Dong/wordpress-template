26bded8eacfdfbfac1de063f7e8a28300761c361
2022Dong <faithhopelove777111@gmail.com>
2024-04-14 16:05:06 +0800

    LOGO upload -customize site identity

--- functions.php
+++ functions.php
@@ -30,6 +30,7 @@ function mytheme_theme_support()
 {
     // Adds dynamic title tag support
     add_theme_support('title-tag');
+    add_theme_support('custom-logo');
 }
 
 add_action('after_setup_theme', 'mytheme_theme_support');
