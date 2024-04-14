c4d123d255ab87ec904aacce24d0e18d270299bb
2022Dong <faithhopelove777111@gmail.com>
2024-04-14 17:30:45 +0800

    add post meta data

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -1,2 +1,25 @@
-<?php
-the_content();
+<div class="container">
+    <header class="content-header">
+        <div class="meta mb-3">
+            <span class="date"><?php the_date(); ?></span>
+            <?php
+            // Output the post tags
+            the_tags('<span class="tag"><i class="fa fa-tag"></i>', '</span><span class="tag"><i class="fa fa-tag"></i>', '</span>');
+            ?>
+            <?php
+            // Output the post categories
+            $categories = get_the_category();
+            if ($categories) {
+                foreach ($categories as $category) {
+                    echo '<span class="tag"><i class="fa fa-tag"></i> ' . $category->name . '</span>';
+                }
+            }
+            ?>
+            <span class="comment"><a href="#comments"><i class='fa fa-comment'></i> <?php comments_number(); ?></a></span>
+        </div>
+    </header>
+
+    <?php
+    the_content();
+    ?>
+</div>
